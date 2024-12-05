0c4ba37c02c2634c9ccc420429e0d42172ceee6d
Bogdan Mircea <bogdan@c.amo.co>
2024-12-05 14:19:49 +0200

    added usage of camino crate

--- tools/rust_analyzer/rust_project.rs
+++ tools/rust_analyzer/rust_project.rs
@@ -3,12 +3,30 @@
 
 use std::collections::{BTreeMap, BTreeSet, HashMap};
 use std::io::ErrorKind;
-use std::path::Path;
 
 use anyhow::anyhow;
+use camino::{Utf8Path, Utf8PathBuf};
 use serde::Serialize;
 
 use crate::aquery::CrateSpec;
+
+/// The format that rust_analyzer expects as a response when automatically invoked.
+#[derive(Debug, Serialize)]
+#[serde(tag = "kind")]
+#[serde(rename_all = "snake_case")]
+pub enum DiscoverProject {
+    Finished {
+        buildfile: Utf8PathBuf,
+        project: RustProject,
+    },
+    Error {
+        error: String,
+        source: Option<String>,
+    },
+    Progress {
+        message: String,
+    },
+}
 
 /// A `rust-project.json` workspace representation. See
 /// [rust-analyzer documentation][rd] for a thorough description of this interface.
@@ -27,6 +45,10 @@ pub struct RustProject {
     /// dependencies as well as sysroot crate (libstd,
     /// libcore and such).
     crates: Vec<Crate>,
+
+    /// The set of runnables, such as tests or benchmarks,
+    /// that can be found in the crate.
+    runnables: Vec<Runnable>,
 }
 
 /// A `rust-project.json` crate representation. See
@@ -74,6 +96,10 @@ pub struct Crate {
     /// For proc-macro crates, path to compiled proc-macro (.so file).
     #[serde(skip_serializing_if = "Option::is_none")]
     proc_macro_dylib_path: Option<String>,
+
+    /// Build information for the crate
+    #[serde(skip_serializing_if = "Option::is_none")]
+    build: Option<Build>,
 }
 
 #[derive(Debug, Default, Serialize)]
@@ -99,7 +125,96 @@ pub struct Dependency {
     name: String,
 }
 
+#[derive(Debug, Serialize)]
+pub struct Build {
+    /// The name associated with this crate.
+    ///
+    /// This is determined by the build system that produced
+    /// the `rust-project.json` in question. For instance, if bazel were used,
+    /// the label might be something like `//ide/rust/rust-analyzer:rust-analyzer`.
+    ///
+    /// Do not attempt to parse the contents of this string; it is a build system-specific
+    /// identifier similar to [`Crate::display_name`].
+    pub label: String,
+    /// Path corresponding to the build system-specific file defining the crate.
+    ///
+    /// It is roughly analogous to [`ManifestPath`], but it should *not* be used with
+    /// [`crate::ProjectManifest::from_manifest_file`], as the build file may not be
+    /// be in the `rust-project.json`.
+    pub build_file: Utf8PathBuf,
+    /// The kind of target.
+    ///
+    /// Examples (non-exhaustively) include [`TargetKind::Bin`], [`TargetKind::Lib`],
+    /// and [`TargetKind::Test`]. This information is used to determine what sort
+    /// of runnable codelens to provide, if any.
+    pub target_kind: TargetKind,
+}
+
+#[derive(Clone, Copy, Debug, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum TargetKind {
+    Bin,
+    /// Any kind of Cargo lib crate-type (dylib, rlib, proc-macro, ...).
+    Lib,
+    Test,
+}
+
+/// A template-like structure for describing runnables.
+///
+/// These are used for running and debugging binaries and tests without encoding
+/// build system-specific knowledge into rust-analyzer.
+///
+/// # Example
+///
+/// Below is an example of a test runnable. `{label}` and `{test_id}`
+/// are explained in [`Runnable::args`]'s documentation.
+///
+/// ```json
+/// {
+///     "program": "bazel",
+///     "args": [
+///         "test",
+///          "{label}",
+///          "--test_arg",
+///          "{test_id}",
+///     ],
+///     "cwd": "/home/user/repo-root/",
+///     "kind": "testOne"
+/// }
+/// ```
+#[derive(Debug, Serialize)]
+pub struct Runnable {
+    /// The program invoked by the runnable.
+    ///
+    /// For example, this might be `cargo`, `bazel`, etc.
+    pub program: String,
+    /// The arguments passed to [`Runnable::program`].
+    ///
+    /// The args can contain two template strings: `{label}` and `{test_id}`.
+    /// rust-analyzer will find and replace `{label}` with [`Build::label`] and
+    /// `{test_id}` with the test name.
+    pub args: Vec<String>,
+    /// The current working directory of the runnable.
+    pub cwd: Utf8PathBuf,
+    pub kind: RunnableKind,
+}
+
+/// The kind of runnable.
+#[derive(Debug, Clone, Copy, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub enum RunnableKind {
+    /// This seems useless in the RA code.
+    Check,
+
+    /// Can run a binary.
+    Run,
+
+    /// Run a single test.
+    TestOne,
+}
+
 pub fn generate_rust_project(
+    workspace: &Utf8Path,
     sysroot: &str,
     sysroot_src: &str,
     crates: &BTreeSet<CrateSpec>,
@@ -108,6 +223,31 @@ pub fn generate_rust_project(
         sysroot: Some(sysroot.into()),
         sysroot_src: Some(sysroot_src.into()),
         crates: Vec::new(),
+        runnables: vec![
+            Runnable {
+                program: "bazel".to_owned(),
+                args: vec!["build".to_owned(), "{label}".to_owned()],
+                cwd: workspace.to_owned(),
+                kind: RunnableKind::Check,
+            },
+            Runnable {
+                program: "bazel".to_owned(),
+                args: vec![
+                    "test".to_owned(),
+                    "{label}".to_owned(),
+                    "--test_output".to_owned(),
+                    "streamed".to_owned(),
+                    "--test_arg".to_owned(),
+                    "--nocapture".to_owned(),
+                    "--test_arg".to_owned(),
+                    "--exact".to_owned(),
+                    "--test_arg".to_owned(),
+                    "{test_id}".to_owned(),
+                ],
+                cwd: workspace.to_owned(),
+                kind: RunnableKind::TestOne,
+            },
+        ],
     };
 
     let mut unmerged_crates: Vec<&CrateSpec> = crates.iter().collect();
@@ -170,6 +310,7 @@ pub fn generate_rust_project(
                     env: Some(c.env.clone()),
                     is_proc_macro: c.proc_macro_dylib_path.is_some(),
                     proc_macro_dylib_path: c.proc_macro_dylib_path.clone(),
+                    build: None,
                 });
             }
         }
@@ -256,9 +397,7 @@ pub fn write_rust_project(
         .to_str()
         .ok_or_else(|| anyhow!("execution_root is not valid UTF-8"))?;
 
-    let output_base = output_base
-        .to_str()
-        .ok_or_else(|| anyhow!("output_base is not valid UTF-8"))?;
+    let output_base = output_base.as_str();
 
     // Try to remove the existing rust-project.json. It's OK if the file doesn't exist.
     match std::fs::remove_file(rust_project_path) {
@@ -294,6 +433,7 @@ mod tests {
     #[test]
     fn generate_rust_project_single() {
         let project = generate_rust_project(
+            "workspace",
             "sysroot",
             "sysroot_src",
             &BTreeSet::from([CrateSpec {
@@ -325,6 +465,7 @@ mod tests {
     #[test]
     fn generate_rust_project_with_deps() {
         let project = generate_rust_project(
+            "workspace",
             "sysroot",
             "sysroot_src",
             &BTreeSet::from([
